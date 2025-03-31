28696c62b05443d5e48d26c7175059bd269ba708
Maha <maha.ahmed4peace@gmail.com>
2025-03-31 13:11:13 -0400

    implementing a StackInterface with additional size() method and implementing it in a ArrayBasedStack<T>

---
+++ cdCollection/src/ArrayBasedStack.java
@@ -0,0 +1,107 @@
+
+public class ArrayBasedStack<T> extends Object implements StackInterface<T> {
+	
+	//T[] type parameters  
+	
+	private T[] stack; // reference to any type of Object 
+	
+	private int top; 
+	
+	//default constructor
+	public ArrayBasedStack() {
+		super(); 
+		top = -1; 
+		stack = (T[]) new Object[10]; 
+	}
+	
+	
+	public ArrayBasedStack(int size) {
+		super();
+		top = -1;
+		if (size > 0) {
+			stack = (T[]) new Object[size];
+		} else {
+			stack = (T[]) new Object[10]; 
+		}
+	}
+	
+	
+	@Override
+	public void push(T item) throws StackFullException  {
+		
+		//boolean pushed = false; 
+		
+		if (top < stack.length - 1) {
+			top++; 
+			stack[top] = item; 
+			//pushed = true; 
+			
+		} else {
+			//System.out.println("Sorry, you cannot add more books"); 
+			//System.out.println is not the best use case because not all users use system.out.println
+			//this creates an object of StackFullException and throws it to where the method was called..in our demo...for example 
+			//Which is where the exception, or unusual event occurred  
+			//Creating an object of type Object, Throwable, Exception, and StackFullException
+			//Once Object is created, it is immediately thrown..a reference for it is not created yet?
+			throw new StackFullException("Pushed attempted on a full stack!"); 
+		}
+		
+		//if (pushed) {
+			//System.out.println("The book has been added.");		}
+		//return pushed; 
+	}
+
+	
+	@Override
+	public void pop() throws StackEmptyException {
+		
+		//boolean popped = false;
+		
+		// checks 
+		
+		if (top >= 0) {
+				stack[top] = null; 
+				top--; 
+				//popped = true; 
+		}
+		//return popped; 
+		else {
+			//Creating an object of type Object, Throwable, Exception, and StackEmptyException
+			throw new StackEmptyException("Pop attempted on an empty stack."); 
+		}
+	}
+
+	
+	@Override
+	public String peek() {
+		//must return type String..
+		//we want to see the top of the stack
+		String topItem; 
+		if (top >= 0) {
+			topItem = stack[top].toString();  
+		} else {
+			topItem = "The Stack is Empty";
+		}
+		return topItem; 
+		
+	}
+
+
+	@Override
+	public int size() {
+		return stack.length; 
+		
+	}
+
+	@Override
+	public String toString(){
+		String allItems = "";
+
+		for(int i = 0; i <= top; i++){
+			allItems = allItems + stack[i]; 
+		}
+
+		return allItems; 
+	}
+
+}
