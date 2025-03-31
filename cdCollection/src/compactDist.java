a76358a38cd3d52cceeb7963d320ddff4f079f89
Maha <maha.ahmed4peace@gmail.com>
2025-03-31 12:52:03 -0400

    New project: cdCollection: building the compactDisc class

---
+++ cdCollection/src/compactDist.java
@@ -0,0 +1,40 @@
+public class compactDist {
+    
+    //instance variables
+    private String artistName;
+    private String albumName;
+    private String albumLanguage; 
+
+    public void setArtistName(String artistName){
+            this.artistName = artistName; 
+
+    }
+
+    public String getArtistName(){
+        return artistName;
+    }
+
+    public void setAlbumName(String albumName){
+        this.albumName = albumName; 
+    }
+
+    public String getAlbumName(){
+        return albumName; 
+    }
+
+    public void setAlbumLanguage(String albumLanguage){
+        this.albumLanguage = albumLanguage; 
+    }
+
+    public String getAlbumLanguage(){
+        return albumLanguage; 
+    }
+
+    public String toString(){
+        return "Artist Name: " + artistName + "\n" + "Album Name: " + albumName + "\n" + albumLanguage; 
+    }
+
+
+
+}
+
